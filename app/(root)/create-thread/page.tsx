11fa04b2310b8d906d634076c265087152dec7f7
Aritra Maji <richard2004@kgpian.iitkgp.ac.in>
2024-07-07 19:43:15 +0530

    My first project

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) redirect("/sign-in");
+
+  // fetch organization list created by user
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <>
+      <h1 className='head-text'>Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
+    </>
+  );
+}
+
+export default Page;
